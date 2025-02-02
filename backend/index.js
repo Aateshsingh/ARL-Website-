03ef2f9a9f7a9015140e7cfffedbe59435e4d644
Nidhish Rathore <codenidhish07@gmail.com>
2025-02-02 12:23:34 +0530

    added backend

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const express = require('express')
+const {connectDB} = require('./db/connection')
+const cors = require('cors')
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+connectDB();
+
+app.use(cors({
+    origin: '*', 
+    methods: 'GET, POST, PUT, DELETE', 
+    allowedHeaders: 'Content-Type, Authorization' 
+  }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(bodyParser.json())
+
+//home route
+app.get("/", (req,res)=>{
+    return res.send("Server is UP and Running...")
+});
+
+
+app.listen(PORT, ()=>console.log("Server Started at PORT: ", PORT));
